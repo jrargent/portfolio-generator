81aa95df55f1c17a84a10d3113b633b97ab33521
Argent <jrargent42@gmail.com>
2022-09-25 14:58:50 -0400

    finished site

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
@@ -126,16 +126,24 @@ Add a New Project
     });
 };
 
-// promptUser()
-//   .then(promptProject)
-//   .then(portfolioData => {
-//     const pageHTML = generatePage(portfolioData);
-
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-
-    // });
-//   });
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 
 const mockData =
@@ -186,7 +194,13 @@ const mockData =
 };
 
 const pageHTML = generatePage(mockData);
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw new Error(err);
 
+fs.writeFile('./dist/index.html', pageHTML, err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Page created! Check out index.html in this directory to see it!');
+
+  
 });
